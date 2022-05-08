bcb3321b9a2c20faa820f42dfbf6add7ff2adbaa
Feláček <petr@ptrk.eu>
2022-05-09 00:33:16 +0200

    Added environment variable support

--- Main.js
+++ Main.js
@@ -1,15 +1,16 @@
 'use strict'
 const Client = require("./js/Client.js")
 const logger = require('tracer').colorConsole();
+require('dotenv').config();
 
 async function main() {
     let client = new Client();
-    const connectionStatus = await client.connect("130.61.125.120", 5000);
+    const connectionStatus = await client.connect(process.env.SERVER_IP, process.env.SERVER_PORT);
     logger.info(connectionStatus)
     const a = await client.call('system.listMethods')
     const b = await client.call('Authenticate', [
-        { value: 'SuperAdmin', type: 'string' },
-        { value: 'oRxfBWw4vj', type: 'string' }
+        { value: process.env.SUPERADMIN_NAME, type: 'string' },
+        { value: process.env.SUPERADMIN_PASSWORD, type: 'string' }
     ])
     const c = await client.call('GetNetworkStats')
     logger.info(a)
