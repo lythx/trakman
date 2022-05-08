ef54a8068d21d80e66303bc6f7a60c634f6ed639
lythx <lythxxx@gmail.com>
2022-05-09 01:10:16 +0200

    hg

--- Main.js
+++ Main.js
@@ -4,17 +4,16 @@ const logger = require('tracer').colorConsole();
 
 async function main() {
     let client = new Client();
-    const connectionStatus = await client.connect("130.61.125.120", 5000);
-    logger.info(connectionStatus)
+    const connectionStatus = await client.connect("130.61.125.120", 5001);
     const a = await client.call('system.listMethods')
     const b = await client.call('Authenticate', [
         { value: 'SuperAdmin', type: 'string' },
         { value: 'oRxfBWw4vj', type: 'string' }
     ])
     const c = await client.call('GetNetworkStats')
-    logger.info(a)
-    logger.warn(b)
-    logger.fatal(c)
+    // logger.info(a)
+    // logger.warn(b)
+    logger.fatal(JSON.stringify(c))
 }
 
 main();
