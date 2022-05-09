280b9409c04b29247a9a74c87c1b89ec9c3136a5
lythx <lythxxx@gmail.com>
2022-05-09 17:46:41 +0200

    fix indent

--- js/Events.js
+++ js/Events.js
@@ -1,13 +1,12 @@
 'use strict'
-const logger = require('tracer').colorConsole();
+const logger = require('tracer').colorConsole()
 
 class Events {
-
-    static handleEvent(name, json) {
-        logger.warn(name)
-        logger.debug(json)
-        //TODO
-    }
+  static handleEvent (name, json) {
+    logger.warn(name)
+    logger.debug(json)
+    // TODO
+  }
 }
 
 module.exports = Events
