3071b73862674824b23b3609b879a7d086245261
lythx <lythxxx@gmail.com>
2022-05-09 12:21:25 +0200

    add comments and tags for public methods

--- js/Client.js
+++ js/Client.js
@@ -10,9 +10,16 @@ class Client {
     response = ''
     awaitingResponse
 
-    async connect(host, port) {
+    /**
+    * Connects to dedicated server and waits for handshake.
+    * Rejects promise if connection is failed or server doesnt use "GBXRemote 2" protocol
+    * @param {String} host ip of dedicated server (default localhost)
+    * @param {Number} port port at which dedicated server is listning for XmlRpc (default 5000)
+    * @returns {Promise<String>} handshake status
+    */
+    async connect(host = 'localhost', port = 5000) {
         return new Promise(async (resolve, reject) => {
-            this.socket.connect(port || 5000, host || 'localhost')
+            this.socket.connect(port, host)
             this.socket.setKeepAlive(true)
             this.socket.setupListeners()
             const handshakeStatus = await this.socket.awaitHandshake()
@@ -23,18 +30,20 @@ class Client {
         })
     }
 
+    /**
+    * Calls a dedicated server method.
+    * @param {String} method dedicated server method name
+    * @param {Object[]} params parameters, each one of objects has to contain type and value parameters
+    * @param {boolean} expectsResponse if set to false doesnt poll the response and returns null.
+    * @returns {Promise<any[]>} array of server response values
+    */
     async call(method, params = [], expectsResponse = true) {
+        this.requestId++                                    //increment requestId so every request has an unique id
         const request = new Request(method, params)
-        const xml = request.getXml()
-        this.requestId++
-        const bufferLength = Buffer.byteLength(xml);
-        const buffer = Buffer.alloc(8 + bufferLength);
-        buffer.writeUInt32LE(bufferLength, 0);
-        buffer.writeUInt32LE(this.requestId, 4);
-        buffer.write(xml, 8);
+        const buffer = request.getPreparedBuffer(this.requestId)
         this.socket.write(buffer);
         if (!expectsResponse)
-            return
+            return null
         const response = await this.socket.awaitResponse(this.requestId);
         return response
     }
