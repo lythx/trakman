65a941657d5097d44ad6651a18541a088a777948
Feláček <petr@ptrk.eu>
2022-05-09 02:37:59 +0200

    If the server returns nothing (the controller failed to connect), exit the program

--- Main.js
+++ Main.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 async function main() {
     let client = new Client();
-    const connectionStatus = await client.connect(process.env.SERVER_IP, process.env.SERVER_PORT);
+    const connectionStatus = await client.connect(process.env.SERVER_IP, process.env.SERVER_PORT)
+        .catch(err => {
+            logger.fatal(err)
+            process.exit(1)
+        });
     logger.info(connectionStatus)
     const a = await client.call('system.listMethods')
     const b = await client.call('Authenticate', [
