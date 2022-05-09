365189d4dc05a22a22debad3f0f62bca79ae039c
lythx <lythxxx@gmail.com>
2022-05-09 23:59:38 +0200

    LOGGER

--- js/Listeners.js
+++ js/Listeners.js
@@ -1,30 +1,27 @@
-import Chat from "./Chat.js"
-import Events from "./Events.js"
-import Logger from "./Logger.js"
+import Chat from './Chat.js'
+import Events from './Events.js'
+import Logger from './Logger.js'
 
 class Listeners {
-
-    #listeners = [
-        {
-            event: 'TrackMania.PlayerConnect',
-            callback: (params) => {
-                Chat.sendMessage(`SUSSY PETYA ${params[0]}`)
-            }
-        },
-        {
-            event: 'TrackMania.PlayerChat',
-            callback: (params) => {
-                Logger.fatal(JSON.stringify(params))
-                //  if (Chat.checkIfCommand(params[2]) && params[0]!==0)
-                //     Chat.handleCommand(params[1], params[2])
-            }
-        }
-    ]
-
-    constructor() {
-        for (const listener of this.#listeners)
-            Events.addListener(listener.event, listener.callback)
+  #listeners = [
+    {
+      event: 'TrackMania.PlayerConnect',
+      callback: (params) => {
+        Chat.sendMessage(`SUSSY PETYA ${params[0]}`)
+      }
+    },
+    {
+      event: 'TrackMania.PlayerChat',
+      callback: (params) => {
+        //  if (Chat.checkIfCommand(params[2]) && params[0]!==0)
+        //     Chat.handleCommand(params[1], params[2])
+      }
     }
+  ]
+
+  initialize () {
+    for (const listener of this.#listeners) { Events.addListener(listener.event, listener.callback) }
+  }
 }
 
-export default Listeners
+export default new Listeners()
