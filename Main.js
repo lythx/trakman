51087334243348b5681706358ea832ada45768a7
lythx <lythxxx@gmail.com>
2022-05-09 22:42:50 +0200

    change to modules, RIP LOGGER

--- Main.js
+++ Main.js
@@ -1,35 +1,33 @@
 'use strict'
-const Client = require('./js/Client.js')
-const logger = require('tracer').colorConsole()
-const Error = require('./js/Error')
-require('dotenv').config()
+import client from './js/Client.js'
+import Logger from './js/Logger.js'
+import Error from './js/Error.js'
+import 'dotenv/config'
 
-async function main () {
-  const client = new Client()
-  logger.trace('Establishing connection with the server...')
+async function main() {
+  Logger.warn('Establishing connection with the server...')
   const connectionStatus = await client.connect(process.env.SERVER_IP, process.env.SERVER_PORT)
     .catch(err => { Error.fatal(err) })
-  logger.info(connectionStatus)
-  logger.trace('Authenticating...')
+  Logger.info(connectionStatus)
+  Logger.trace('Authenticating...')
   const authenticationStatus = await client.call('Authenticate', [
     { string: process.env.SUPERADMIN_NAME },
     { string: process.env.SUPERADMIN_PASSWORD }
   ])
   if (authenticationStatus[0].faultCode) { Error.fatal('Authentication failed', authenticationStatus[0].faultString, authenticationStatus[0].faultCode) }
-  logger.info('Authentication success')
-  logger.trace('Enabling callbacks...')
+  Logger.info('Authentication success')
+  Logger.trace('Enabling callbacks...')
   const enableCallbacks = await client.call('EnableCallbacks', [
     { boolean: true }
   ])
   if (enableCallbacks[0].faultCode) { Error.fatal('Failed to enable callbacks', enableCallbacks[0].faultString, enableCallbacks[0].faultCode) }
-  logger.info('Callbacks enabled')
-  logger.trace('Fetching challenges...')
+  Logger.info('Callbacks enabled')
+  Logger.trace('Fetching challenges...')
   const challengeList = await client.call('GetChallengeList', [
     { int: 5000 }, { int: 0 }
   ])
   if (challengeList[0].faultCode) { Error.fatal('Unable to fetch challenge list', challengeList[0].faultString, challengeList[0].faultCode) }
-  logger.info('Challenges fetched')
-  // process.exit(0);
+  Logger.info('Challenges fetched')
 }
 
 main()
