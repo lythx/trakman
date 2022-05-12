43819b85e58cd84b251654baf275dcd4b59df3e4
lythx <lythxxx@gmail.com>
2022-05-12 20:56:11 +0200

    this isnt working just need to get it to main pc

--- Main.js
+++ Main.js
@@ -1,33 +1,32 @@
 'use strict'
 import Client from './js/Client.js'
 import Logger from './js/Logger.js'
-import Error from './js/Error.js'
 import ChallengeService from './js/services/ChallengeService.js'
 import 'dotenv/config'
 import Listeners from './js/Listeners.js'
 import DefaultCommands from './js/plugins/DefaultCommands.js'
 import Players from './js/Players.js'
+import ErrorHandler from './js/ErrorHandler.js'
 
 async function main () {
   Logger.warn('Establishing connection with the server...')
   const connectionStatus = await Client.connect(process.env.SERVER_IP, process.env.SERVER_PORT)
-    .catch(err => { Error.fatal(err) })
+    .catch(err => { ErrorHandler.fatal(err) })
   Logger.info(connectionStatus)
   Logger.trace('Authenticating...')
-  const authenticationStatus = await Client.call('Authenticate', [
+  await Client.call('Authenticate', [
     { string: process.env.SUPERADMIN_NAME },
     { string: process.env.SUPERADMIN_PASSWORD }
-  ])
-  if (authenticationStatus[0].error) { Error.fatal('Authentication failed', authenticationStatus[0].errorString, authenticationStatus[0].errorCode) }
+  ]).catch(err => ErrorHandler.fatal('Server authentication failed', err.errorString, err.errorCode))
+
   Logger.info('Authentication success')
   Listeners.initialize()
   const defaultCommands = new DefaultCommands()
   defaultCommands.initialize()
   Logger.trace('Enabling callbacks...')
-  const enableCallbacks = await Client.call('EnableCallbacks', [
+  await Client.call('EnableCallbacks', [
     { boolean: true }
-  ])
-  if (enableCallbacks[0].error) { Error.fatal('Failed to enable callbacks', enableCallbacks[0].errorString, enableCallbacks[0].errorCode) }
+  ]).catch(err => { ErrorHandler.fatal('Failed to enable callbacks', err.errorString, err.errorCode) })
   Logger.info('Callbacks enabled')
   Logger.trace('Fetching challenges...')
   const challengeService = new ChallengeService()
@@ -36,7 +35,8 @@ async function main () {
   await challengeService.push()
     .then(() => Logger.info('Challenges are in the database'))
   await Players.initialize()
-    .then(() => Logger.info('Player service instantiated'))
+    .catch(err => ErrorHandler.error(err))
+  Logger.info('Player service instantiated')
 }
 
 main()
