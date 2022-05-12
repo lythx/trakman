ac2e7744ec85912702f304f1621dce20259afd3c
wsrvn <105176370+wsrvn@users.noreply.github.com>
2022-05-12 09:45:53 +0300

    add colour table
    literally

---
+++ js/data/Colours.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const colours = {
+    white: '$fff',
+    black: '$000',
+    grey: '$555',
+    red: '$f00',
+    orange: '$f50',
+    deeppink: '$f05',
+    green: '$0f0',
+    limegreen: '$5f0',
+    lightgreen: '$0f5',
+    blue: '$00f',
+    deeppurple: '$50f',
+    blurple: '$05f',
+    yellow: '$ff0',
+    cyan: '$0ff',
+    magenta: '$f0f',
+}
+
+// ADD MORE IF NEEDED
+// FOR COLOUR NAMES SEARCH WIKI OR MAKE THEM UP I DONT CARE
+
+export default colours
