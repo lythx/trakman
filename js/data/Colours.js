0aaa61936c7b6ef701e6220db1ffd1924a789867
wsrvn <vibex2new@gmail.com>
2022-05-12 10:00:27 +0300

    fix format

--- js/data/Colours.js
+++ js/data/Colours.js
@@ -1,21 +1,21 @@
 'use strict'
 
 const colours = {
-    white: '$fff',
-    black: '$000',
-    grey: '$555',
-    red: '$f00',
-    orange: '$f50',
-    deeppink: '$f05',
-    green: '$0f0',
-    limegreen: '$5f0',
-    lightgreen: '$0f5',
-    blue: '$00f',
-    deeppurple: '$50f',
-    blurple: '$05f',
-    yellow: '$ff0',
-    cyan: '$0ff',
-    magenta: '$f0f',
+  white: '$fff',
+  black: '$000',
+  grey: '$555',
+  red: '$f00',
+  orange: '$f50',
+  deeppink: '$f05',
+  green: '$0f0',
+  limegreen: '$5f0',
+  lightgreen: '$0f5',
+  blue: '$00f',
+  deeppurple: '$50f',
+  blurple: '$05f',
+  yellow: '$ff0',
+  cyan: '$0ff',
+  magenta: '$f0f'
 }
 
 // ADD MORE IF NEEDED
