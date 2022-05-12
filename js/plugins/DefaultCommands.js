a49f6cbf99855831cf6ad83d093d94e7b6c04677
wsrvn <105176370+wsrvn@users.noreply.github.com>
2022-05-12 09:46:49 +0300

    add colour test to default coms

--- js/plugins/DefaultCommands.js
+++ js/plugins/DefaultCommands.js
@@ -3,26 +3,27 @@ import Command from '../Command.js'
 import Logger from '../Logger.js'
 import Events from '../Events.js'
 import Client from '../Client.js'
+import colours from '../data/Colours.js'
 
 class DefaultCommands {
-  #commands = [
-    //THIS IS JUST A TEST PLEASE FIX THIS FOR PRODUCTION
-    new Command(
-      ['qwe', 'qwer', '123', 'test'],
-      'qqweqwe',
-      () => { Client.call('ChatSendServerMessage', [{ string: '$f0fqwrqwerwe' }], false) }
-    )
-  ]
+    #commands = [
+        //THIS IS JUST A TEST PLEASE FIX THIS FOR PRODUCTION
+        new Command(
+            ['qwe', 'qwer', '123', 'test'],
+            'qqweqwe',
+            () => { Client.call('ChatSendServerMessage', [{ string: `${colours.yellow}qwrqwerwe` }], false) }
+        )
+    ]
 
-  initialize () {
-    for (const command of this.#commands) {
-      Events.addListener('TrackMania.PlayerChat', async (params) => {
-        if (params[0] === 0 || !command.aliases.some(a => (params[2].trim().toLowerCase()).split(' ')[0] === `/${a}`)) { return }
-        // TODO check for privileges db query
-        command.callback(/*TODO::: PASS PLAYER INFO THIS IS CRITICAL FOR OUR MISSION*/(params[2].trim()).split(/ /).shift())
-      })
+    initialize() {
+        for (const command of this.#commands) {
+            Events.addListener('TrackMania.PlayerChat', async (params) => {
+                if (params[0] === 0 || !command.aliases.some(a => (params[2].trim().toLowerCase()).split(' ')[0] === `/${a}`)) { return }
+                // TODO check for privileges db query
+                command.callback(/*TODO::: PASS PLAYER INFO THIS IS CRITICAL FOR OUR MISSION*/(params[2].trim()).split(/ /).shift())
+            })
+        }
     }
-  }
 }
 
 export default DefaultCommands
