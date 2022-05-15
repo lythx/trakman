7892ee8b6f0082bb98a3e32b9d1a2224ff0d9368
lythx <lythxxx@gmail.com>
2022-05-15 12:56:46 +0200

    comamnds

---
+++ plugins/DefaultCommands.ts
@@ -0,0 +1,28 @@
+'use strict'
+import { Client } from '../src/Client.js'
+import colours from '../src/data/Colours.json' assert {type: 'json'}
+import { ChatService } from '../src/services/ChatService.js'
+
+const commands: Command[] = [
+  // THIS IS JUST A TEST PLEASE FIX THIS FOR PRODUCTION
+  {
+    aliases: ['qwe', 'qwer', '123', 'test'],
+    help: 'qqweqwe',
+    callback: async () => {
+      await Client.call('ChatSendServerMessage', [{ string: `${colours.yellow}qwrqwerwe` }], false)
+    },
+    level: 0
+  },
+  {
+    aliases: ['ct', 'colourtest'],
+    help: 'test the colours',
+    callback: async () => {
+      const col = Object.values(colours)
+      await Client.call('ChatSendServerMessage', [{ string: col.map((v) => `${v}|`).join(' ') }], false)
+    },
+    level: 0
+  }
+]
+
+for (const command of commands)
+  ChatService.addCommand(command)
