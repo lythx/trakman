c3d3b1420428568e25e0379e0b31f743c6277585
wsrvn <vibex2new@gmail.com>
2022-05-24 02:55:23 +0300

    customui set on server startup

--- plugins/UI.ts
+++ plugins/UI.ts
@@ -2,19 +2,31 @@
 import { TRAKMAN as TM } from '../src/Trakman.js'
 
 const plugins: TMEvent[] = [
-    // {
-    //     event: 'Controller.PlayerJoin',
-    //     callback:async (player: TMPlayer) => {
-    //         console.log('dsfafdsfsdsdf')
-    //         TM.sendManialink(`
-    //         <manialink id="8088084">
-    //         <frame posn="15 15 15">
-    //         <quad posn="0 0 0" sizen="10 10" halign="center" valign="center" style="MedalsBig" substyle="MedalGold" action="8088085"/>
-    //         </frame></manialink>`)
-    //     }
-    // }
+    {
+        event: 'Controller.Ready',
+        callback: async () => {
+            const customUi: string =
+                `<manialinks><manialink id="0"><line></line></manialink><custom_ui>`
+                + `<notice visible="false"/>`
+                + `<challenge_info visible="false"/>`
+                + `<net_infos visible="true"/>`
+                + `<chat visible="true"/>`
+                + `<checkpoint_list visible="true"/>`
+                + `<round_scores visible="true"/>`
+                + `<scoretable visible="true"/>`
+                + `<global visible="true"/>`
+                + `</custom_ui></manialinks>`
+            await TM.call('SendDisplayManialinkPage', [{ string: customUi }, { int: 0 }, { boolean: false }])
+        }
+    },
+    {
+        event: 'TrackMania.BeginChallenge', // Need a Controller event for better handling
+        callback: async (params: any[]) => {
+
+        }
+    },
 ]
 
 for (const plugin of plugins) {
     TM.addListener(plugin.event, plugin.callback)
-  }
+}
