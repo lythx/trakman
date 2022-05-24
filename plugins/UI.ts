10d831fad2f5aeb051f76303a4dde7791f474d7f
wsrvn <vibex2new@gmail.com>
2022-05-24 04:34:59 +0300

    add 5059 todos

--- plugins/UI.ts
+++ plugins/UI.ts
@@ -1,28 +1,126 @@
 'use strict'
 import { TRAKMAN as TM } from '../src/Trakman.js'
 
+// THIS IS A BIG, HUGE, GIGANTIC TODO FOR NOW!!!
+// EVENT DESCRIPTIONS TAKEN FROM RE/EYEPIECE
+
 const plugins: TMEvent[] = [
     {
         event: 'Controller.Ready',
         callback: async () => {
-            const customUi: string =
+            const customUi: string = // Custom UI settings
                 `<manialinks><manialink id="0"><line></line></manialink><custom_ui>`
-                + `<notice visible="false"/>`
-                + `<challenge_info visible="false"/>`
-                + `<net_infos visible="true"/>`
-                + `<chat visible="true"/>`
-                + `<checkpoint_list visible="true"/>`
-                + `<round_scores visible="true"/>`
-                + `<scoretable visible="true"/>`
-                + `<global visible="true"/>`
+                + `<notice visible="false"/>` // Notice in the top left
+                + `<challenge_info visible="false"/>` // Challenge info in the top right
+                + `<net_infos visible="true"/>` // Loading bar in the top right
+                + `<chat visible="true"/>` // Chat
+                + `<checkpoint_list visible="true"/>` // Checkpoint list, right of timer
+                + `<round_scores visible="true"/>` // Round scores in rounds mode, right side
+                + `<scoretable visible="true"/>` // Scoretable on podium/score
+                + `<global visible="true"/>` // All the windows: speed, timer, prev/best, etc.
                 + `</custom_ui></manialinks>`
             await TM.call('SendDisplayManialinkPage', [{ string: customUi }, { int: 0 }, { boolean: false }])
+            // TODO: SetForceMods
+        }
+    },
+    {
+        event: 'Controller.PlayerJoin',
+        callback: async (player: TMPlayer) => {
+            // TODO: Fetch the connecting player info
+
+            // TODO: Fetch player records on the current challenge
+            // TODO: Display all the widgets for the new player
+            // Preferably with an indicator if they have a record
+        }
+    },
+    {
+        event: 'Controller.PlayerLeave',
+        callback: async (playerInfo: PlayerInfo) => {
+            // TODO: Update the widgets to no more indicate the disconnectee's presence
+            // That is, if they had any records
+
+            // TODO: Update miscellaneous widgets:
+            // Ranking, players/specs...
+        }
+    },
+    {
+        event: 'TrackMania.PlayerFinish', // Need a Controller event, PlayerRecord doesn't fit IMO
+        callback: async (params: any[]) => {
+            // TODO: Update cpcounter to indicate finish
+        }
+    },
+    {
+        event: 'TrackMania.PlayerInfoChanged', // Need a Controller event for better handling
+        callback: async (params: any[]) => {
+            // TODO: Remove cpcounter if player switched to specmode
+            // PlayerInfo['SpectatorStatus'] % 10 !== 0
+
+            // TODO: Update miscellaneous widgets:
+            // Ranking, players/specs...
+        }
+    },
+    {
+        event: 'TrackMania.PlayerManialinkPageAnswer', // Need a Controller event for better handling
+        callback: async (params: any[]) => {
+            // This will basically handle every widget click
+            // If I were to write every TODO I'd kill myself, so..
+            // TODO: Everything about players <-> widgets interaction
+        }
+    },
+    {
+        event: 'Controller.DedimaniaRecords',
+        callback: async (params: any[]) => { // Should return TMDedi[]
+            // TODO: Fill in the Dedimania record widget
+        }
+    },
+    {
+        event: 'Controller.PlayerDediRecord', // Not a thing yet
+        callback: async (params: any[]) => { // Should return TMRecord (TMDedi?)
+            // TODO: Update the Dedimania widget
+        }
+    },
+    {
+        event: 'Controller.PlayerRecord',
+        callback: async (params: any[]) => { //Should return TMRecord
+            // TODO: Update the local records widget
+        }
+    },
+    {
+        event: 'TrackMania.ChallengeListModified', // Need a Controller event for better handling
+        callback: async (params: any[]) => {
+            // TODO: Re-fetch the next challenge info
+            // TODO: Update miscellaneous widgets:
+            // Trackcount...
+        }
+    },
+    {
+        event: 'TrackMania.Checkpoint', // Need a Controller event for better handling
+        callback: async (params: any[]) => {
+            // TODO: Update cpcounter to indicate current cp
+        }
+    },
+    {
+        event: 'TrackMania.EndChallenge', // Need a Controller event for better handling
+        callback: async (params: any[]) => {
+            // TODO: Remove race widgets
+
+            // TODO: Display all the podium/score widgets
         }
     },
     {
         event: 'TrackMania.BeginChallenge', // Need a Controller event for better handling
         callback: async (params: any[]) => {
+            // TODO: Remove podium/score widgets
 
+            // TODO: Fetch the current challenge info
+            // TODO: Fetch the next challenge info
+
+            // TODO: Display current challenge widget
+            // TODO: Display current challenge record widgets
+
+            // TODO: Display the miscellaneous widgets:
+            // Clock, addfav, cpcounter, gamemode, visitors,
+            // TMX info, trackcount, ranking, players/specs..
         }
     },
 ]
