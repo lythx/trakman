d401e604d9e702a155a3ec1e3c6f51dc4dfa4c90
wsrvn <vibex2new@gmail.com>
2022-05-25 22:25:01 +0300

    fix tmx mesasge+chat

--- src/commands/TMX.ts
+++ src/commands/TMX.ts
@@ -7,31 +7,32 @@ const command: TMCommand = {
   aliases: ['add'],
   help: 'Add a track from TMX.',
   callback: async (info: MessageInfo) => {
-    const [trackId, game] = info.text.split(' ')
+    const [trackId, game] = info.text.toUpperCase().split(' ')
     const file = await TMXService.fetchTrackFile(trackId, game).catch((err: Error) => err)
     if (file instanceof Error) {
-      TM.sendMessage(`Failed to fetch map file from ${game || 'TMNF'} TMX, check if you specified the correct game.`, info.login)
+      TM.sendMessage(`${TM.colours.yellow}»${TM.colours.red} Failed to fetch file from ${TM.colours.white + game || 'TMNF'} TMX` +
+        `${TM.colours.red}, check if you specified the correct game.`, info.login)
       return
     }
     const write = await Client.call('WriteFile', [{ string: file.name }, { base64: file.content }])
     if (write instanceof Error) {
-      TM.sendMessage('Server failed to write file')
+      TM.sendMessage(`${TM.colours.yellow}»${TM.colours.red} Server failed to write file.`, info.login)
       return
     }
     const insert = await Client.call('InsertChallenge', [{ string: file.name }])
     if (insert instanceof Error) {
-      TM.sendMessage('Server failed to add challenge to jukebox')
+      TM.sendMessage(`${TM.colours.yellow}»${TM.colours.red} Server failed to queue the challenge.`, info.login)
       return
     }
     const insertRes = await Client.call('GetNextChallengeInfo')
     if (insertRes instanceof Error) {
-      TM.sendMessage('Failed to fetch next challenge info')
+      TM.sendMessage(`${TM.colours.yellow}»${TM.colours.red} Server failed to fetch the challenge info.`, info.login)
       return
     }
     const name = insertRes[0].Name
     TM.sendMessage(`${TM.colours.yellow}»» ${TM.colours.folly}${TM.getTitle(info)} ` +
-        `${TM.colours.white + TM.stripModifiers(info.nickName, true)}${TM.colours.folly} has added and queued ` +
-        `${TM.colours.white + TM.stripModifiers(name, true)}${TM.colours.folly} from TMX.`)
+      `${TM.colours.white + TM.stripModifiers(info.nickName, true)}${TM.colours.folly} has added and queued ` +
+      `${TM.colours.white + TM.stripModifiers(name, true)}${TM.colours.folly} from TMX.`)
   },
   privilege: 1
 }
