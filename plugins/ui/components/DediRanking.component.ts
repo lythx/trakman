2a8c1fc016bdf212ac9f518da002d8d51cc710cf
lythx <lythxxx@gmail.com>
2022-06-06 15:11:38 +0200

    suspicious dedimania ui

--- plugins/ui/components/DediRanking.component.ts
+++ plugins/ui/components/DediRanking.component.ts
@@ -58,62 +58,96 @@ export default class DediRanking extends StaticComponent implements IStaticCompo
     const titleWidth = CFG.dediRecordsWidget.width - 0.8
     const side: boolean = (CFG.dediRecordsWidget.posX < 0) ? true : false
     let xml = `<frame posn="0 -3 10">`
-    let textColour: string
-    const pr = TM.dediRecords.find(a => a.login === login)
-    const prIndex = pr !== undefined ? TM.dediRecords.indexOf(pr) : Infinity
+    const playerDedi = TM.dediRecords.find(a => a.login === login)
+    const playerDediIndex = playerDedi !== undefined ? TM.dediRecords.indexOf(playerDedi) : Infinity
+    let personalStart = playerDediIndex > TM.dediRecords.length - Math.ceil((CFG.dediRecordsWidget.entries - CFG.dediRecordsWidget.topCount) / 2) ?
+      TM.dediRecords.length - (CFG.dediRecordsWidget.entries - CFG.dediRecordsWidget.topCount) :
+      playerDediIndex - Math.floor((CFG.dediRecordsWidget.entries - CFG.dediRecordsWidget.topCount) / 2)
+    if (playerDediIndex === Infinity) { personalStart++ }
+    let displayIndex = 0 //display index is number of records that were displayed
     for (const [i, p] of TM.dediRecords.entries()) {
-      if (i < prIndex) {
-        if (i >= CFG.dediRecordsWidget.topCount) {
-          textColour = CFG.widgetStyleRace.colours.better
-        } else {
-          textColour = CFG.widgetStyleRace.colours.top
-        }
-      } else if (i > prIndex) {
-        if (i >= CFG.dediRecordsWidget.topCount) {
-          textColour = CFG.widgetStyleRace.colours.worse
-        } else {
-          textColour = CFG.widgetStyleRace.colours.top
-        }
-      } else {
-        textColour = CFG.widgetStyleRace.colours.self
-      }
-      xml += // Records list in XML
+      if (i >= CFG.dediRecordsWidget.topCount && i < personalStart)
+        continue
+      const textColour = this.getTextColour(i, playerDediIndex)
+      xml += // Record in XML
         `<format textsize="1" textcolor="${CFG.widgetStyleRace.colours.default}"/>
-        <label posn="2.3 ${-1.8 * i} 0.04" sizen="1.7 1.7" scale="0.9" halign="right" 
+        <label posn="2.3 ${-1.8 * displayIndex} 0.04" sizen="1.7 1.7" scale="0.9" halign="right" 
          text="${CFG.widgetStyleRace.formattingCodes}${i + 1}."/>
-        <label posn="5.9 ${-1.8 * i} 0.04" sizen="3.8 1.7" scale="0.9" halign="right" 
+        <label posn="5.9 ${-1.8 * displayIndex} 0.04" sizen="3.8 1.7" scale="0.9" halign="right" 
          textcolor="${textColour}" text="${CFG.widgetStyleRace.formattingCodes + TM.Utils.getTimeString(p.score)}"/>
-        <label posn="6.1 ${(-1.8 * i) + 0.05} 0.04" sizen="${CFG.dediRecordsWidget.width - 5.7} 1.7" scale="0.9" 
+        <label posn="6.1 ${(-1.8 * displayIndex) + 0.05} 0.04" sizen="${CFG.dediRecordsWidget.width - 5.7} 1.7" scale="0.9" 
          text="${CFG.widgetStyleRace.formattingCodes + TM.strip(TM.safeString(p.nickName), false)}"/>`
       // Indicate online players
-      if (TM.getPlayer(p.login) !== undefined) {
-        // Amount of records is bigger than max top entries (nullcheck)
-        if (i > CFG.dediRecordsWidget.topCount) {
-          // Player's record is slower than the worst top entry
-          // Add line indicating player position
-          xml +=
-            `<quad posn="0.4 ${-1.8 * i + 0.3} 0.03" sizen="${titleWidth} ${1.8 + 0.3}" 
+      if (TM.getPlayer(p.login) !== undefined) { // Amount of records is bigger than max top entries (nullcheck)
+        if (i > CFG.dediRecordsWidget.topCount) { // If this entry is inside the top records dont't add background shade as it would be doubled
+          xml += // Add line indicating player position
+            `<quad posn="0.4 ${-1.8 * displayIndex + 0.3} 0.03" sizen="${titleWidth} ${1.8 + 0.3}" 
              style="${CFG.widgetStyleRace.hlSelfStyle}" substyle="${CFG.widgetStyleRace.hlSelfSubStyle}"/>`
         }
-        // Add marker
-        xml +=
-          `<quad posn="${side ? 15.4 : -1.9} ${-1.8 * i + 0.3} 0.04" sizen="2 2" 
+        xml += // Add marker
+          `<quad posn="${side ? 15.4 : -1.9} ${-1.8 * displayIndex + 0.3} 0.04" sizen="2 2" 
            style="${CFG.widgetStyleRace.hlSelfStyle}" substyle="${CFG.widgetStyleRace.hlSelfSubStyle}"/>
-          <quad posn="${side ? 15.6 : -1.7} ${-1.8 * i + 0.1} 0.05" sizen="1.6 1.6" `
-        if (i < prIndex) {
+          <quad posn="${side ? 15.6 : -1.7} ${-1.8 * displayIndex + 0.1} 0.05" sizen="1.6 1.6" `
+        if (i < playerDediIndex) { // Player faster than your record
           xml += `style="Icons128x128_1" substyle="ChallengeAuthor"/>`
-        } else if (i > prIndex) {
+        } else if (i > playerDediIndex) { // Player slower than your record
           xml += `style="Icons128x128_1" substyle="Solo"/>`
-        } else {
+        } else { // Your record
           xml += `style="Icons64x64_1" substyle="${side ? 'ArrowPrev' : 'ArrowNext'}"/>`
         }
       }
-      // Add no record thing if no record from player
-      if (i === CFG.dediRecordsWidget.entries - 1) {
-        break
+      displayIndex++
+      if (displayIndex === CFG.dediRecordsWidget.entries || (playerDediIndex === Infinity && displayIndex === CFG.dediRecordsWidget.entries - 1)) {
+        break // Break if theres max entries, leave one entry empty if player doesn't have a record
       }
+    }
+    // Add empty entry at end if player has no record
+    if (playerDediIndex === Infinity) {
+      const p = TM.getPlayer(login)
+      if (p === undefined) { // VERY unlikely to happen
+        TM.error(`Cannot find player ${login} in memory.`)
+        return `<frame posn="0 -3 10"></frame>`
+      }
+      // If this entry is inside the top records dont't add background shade as it would be doubled
+      const background = TM.dediRecords.length < CFG.dediRecordsWidget.topCount + 1 ? '' :
+        `<quad posn="0.4 ${-1.8 * displayIndex + 0.3} 0.03" sizen="${titleWidth} ${1.8 + 0.3}" 
+         style="${CFG.widgetStyleRace.hlSelfStyle}" substyle="${CFG.widgetStyleRace.hlSelfSubStyle}"/>`
+      xml +=
+        `<format textsize="1" textcolor="${CFG.widgetStyleRace.colours.default}"/>
+        <label posn="2.3 ${-1.8 * displayIndex} 0.04" sizen="1.7 1.7" scale="0.9" halign="right" 
+         text="${CFG.widgetStyleRace.formattingCodes}--."/>
+        <label posn="5.9 ${-1.8 * displayIndex} 0.04" sizen="3.8 1.7" scale="0.9" halign="right" 
+         textcolor="${CFG.widgetStyleRace.colours.self}" text="-:--.--"/>
+        <label posn="6.1 ${(-1.8 * displayIndex) + 0.05} 0.04" sizen="${CFG.dediRecordsWidget.width - 5.7} 1.7" scale="0.9" 
+         text="${CFG.widgetStyleRace.formattingCodes + TM.strip(TM.safeString(p?.nickName), false)}"/>
+        ${background}
+        <quad posn="${side ? 15.4 : -1.9} ${-1.8 * displayIndex + 0.3} 0.04" sizen="2 2" 
+         style="${CFG.widgetStyleRace.hlSelfStyle}" substyle="${CFG.widgetStyleRace.hlSelfSubStyle}"/>
+        <quad posn="${side ? 15.6 : -1.7} ${-1.8 * displayIndex + 0.1} 0.05" sizen="1.6 1.6" style="Icons64x64_1" substyle="${side ? 'ArrowPrev' : 'ArrowNext'}"/>`
     }
     xml += '</frame>'
     return xml
   }
+
+  /**
+   * Get time color depending on position
+   */
+  private getTextColour(dediIndex: number, playerDediIndex: number): string {
+    if (dediIndex < playerDediIndex) { // Player faster than your record
+      if (dediIndex >= CFG.dediRecordsWidget.topCount) {
+        return CFG.widgetStyleRace.colours.better
+      } else { // Player is in top records
+        return CFG.widgetStyleRace.colours.top
+      }
+    } else if (dediIndex > playerDediIndex) { // Player slower than your record
+      if (dediIndex >= CFG.dediRecordsWidget.topCount) {
+        return CFG.widgetStyleRace.colours.worse
+      } else { // Player is in top records
+        return CFG.widgetStyleRace.colours.top
+      }
+    } else { // Your record 
+      return CFG.widgetStyleRace.colours.self
+    }
+  }
+
 }
