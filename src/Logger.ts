e4442b74d32eef25a28adc028caca0431239b408
lythx <lythxxx@gmail.com>
2022-07-13 11:49:20 +0200

    logger

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,10 +1,29 @@
+import fs from 'fs/promises'
+
+type Tag = 'Log' | 'Warn' | 'Fatal' | 'Debug' | 'Error' | 'Trace' | 'Info'
+
+// TODO: make logs appearance and file locations customizable in config
+
 export class Logger {
+
+  static readonly tagConsoleColours = {
+    Warn: '[33m',
+    Fatal: '[31m',
+    Error: '[31m',
+    Trace: '[35m',
+    Info: '[32m',
+    Debug: '[36m'
+  } as const
+
   static log(str: string): void {
     console.log(`<Log>${str}`)
   }
 
   static warn(str: string): void {
-    console.log(`\u001b[33m<Warn>${str}\x1b[0m`)
+    const date = new Date().toUTCString()
+    const s = `<Warn> [${date.substring(5, date.length - 4)}] ${str}`
+    fs.appendFile('./logs/warnLog.txt', s)
+    console.log(`\u001b[33m${s}\x1b[0m`)
   }
 
   static fatal(str: string): void {
@@ -12,7 +31,8 @@ export class Logger {
   }
 
   static error(str: string): void {
-    console.log(`\u001b[31m$<Fatal>${str}\x1b[0m`)
+    fs.appendFile('./logs/errorLog.txt', `<${new Date().toUTCString()}>${str}`)
+    console.log(`\u001b[31m<Fatal>${str}\x1b[0m`)
   }
 
   static trace(str: string): void {
@@ -26,4 +46,19 @@ export class Logger {
   static debug(str: string): void {
     console.log(`\u001b[36m<Debug>${str}\x1b[0m`)
   }
+
+  private static formatString(tag: Tag, str: string): string {
+    const date = new Date().toUTCString()
+    const colour = (this.tagConsoleColours as any)[tag]
+    const colourString = colour === undefined ? `\u001b${colour}` : ``
+    return `${colourString}<${tag}> [${date.substring(5, date.length - 4)}] ${str}\x1b[0m`
+  }
+
 }
+
+process.on('uncaughtException', async (err: Error) => {
+  Logger.fatal('Uncaught exception occured: ')
+  console.log(err.message + '\n' + err?.stack)
+  await fs.appendFile('./logs/crashLog.txt', `${new Date().toUTCString()}\n${err.message}\n${err?.stack}\n\n`)
+  process.exit(1)
+})
