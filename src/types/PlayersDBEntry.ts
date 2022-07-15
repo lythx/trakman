de4cd43f16e58cbd017e12c5f56e8e5ba126ba45
lythx <lythxxx@gmail.com>
2022-07-16 00:14:14 +0200

    player repository fixes

---
+++ src/types/PlayersDBEntry.ts
@@ -0,0 +1,9 @@
+interface PlayersDBEntry {
+  readonly login: string
+  readonly nickname: string
+  readonly nation: string
+  readonly wins: number
+  readonly timePlayed: number
+  readonly privilege: number
+  readonly visits: number
+}
