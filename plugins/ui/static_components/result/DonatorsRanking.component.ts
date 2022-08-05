0b3a42e6546212bc58618418858dce9a5db043d0
lythx <lythxxx@gmail.com>
2022-08-05 11:43:47 +0200

    working donators stuf

--- plugins/ui/static_components/result/DonatorsRanking.component.ts
+++ plugins/ui/static_components/result/DonatorsRanking.component.ts
@@ -8,28 +8,22 @@ export default class DonatorsRanking extends StaticComponent {
   private readonly width = CFG.static.width
   private readonly height = CFG.donatorsRanking.height
   private readonly entries: number
-  private readonly posX = CFG.static.rightPosition - (CFG.static.width + CFG.marginBig)
+  private readonly posX = CFG.static.rightPosition - (CFG.static.width + CFG.marginBig) * 2
   private readonly posY = CFG.static.topBorder
   private readonly list: List
   private readonly side = CFG.donatorsRanking.side
   private xml = ''
-  private readonly ranking: { nickname: string, amount: number }[] = []
+  private ranking: { nickname: string, amount: number }[] = []
 
   constructor() {
     super(IDS.donatorsRanking, 'result')
     this.entries = CFG.donatorsRanking.entries
     this.list = new List(this.entries, this.width, this.height - (CFG.staticHeader.height + CFG.marginSmall), CFG.donatorsRanking.columnProportions as any, { background: CFG.static.bgColor, headerBg: CFG.staticHeader.bgColor })
     this.constructXml()
+    this.ranking = Donations.topDonators
+    
     TM.addListener('Controller.EndMap', async () => {
-      const res = await TM.queryDB(`SELECT nickname, time_played FROM players
-      JOIN player_ids ON player_ids.id=players.id
-      ORDER BY time_played DESC
-      LIMIT ${this.entries + 1}`)
-      if (res instanceof Error) {
-        return ///////
-      }
-      this.ranking.push(...res.map(a => ({ nickname: a.nickname, amount: Math.round(a.time_played / (60 * 60 * 1000)) })))
-      this.display()
+      this.ranking = Donations.topDonators
     })
     TM.addListener('Controller.BeginMap', () => {
       this.ranking.length = 0
