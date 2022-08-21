2e69e99b8b01498c38e5d18abdc1cae7217b6605
lythx <lythxxx@gmail.com>
2022-08-21 11:11:42 +0200

    add listeners and fix types in dedi plugin

--- plugins/dedimania/DedimaniaTypes.ts
+++ plugins/dedimania/DedimaniaTypes.ts
@@ -1,7 +1,34 @@
-interface TMDedi {
+export interface DediRecord {
   readonly login: string
   nickname: string
   time: number
   checkpoints: number[]
 }
 
+export type NewDediRecord = Omit<TMPlayer & DediRecord & {
+  readonly position: number
+  readonly previousPosition: number
+  readonly previousTime: number
+}, 'currentCheckpoints' | 'isSpectator'>
+
+interface f {
+  readonly map: string
+  readonly login: string
+  readonly time: number
+  readonly checkpoints: number[]
+  readonly nickname: string
+  readonly country: string
+  readonly countryCode: string
+  readonly timePlayed: number
+  readonly joinTimestamp: number
+  readonly wins: number
+  readonly privilege: number
+  readonly visits: number
+  readonly position: number
+  readonly previousPosition: number
+  readonly previousTime: number
+  readonly playerId: number
+  readonly ip: string
+  readonly region: string
+  readonly isUnited: boolean
+}
