a5934735e34cb29886b1b94e9185e800c7a88488
lythx <lythxxx@gmail.com>
2022-08-24 15:38:36 +0200

    mk plugin unfinished

---
+++ plugins/maniakarma/ManiakarmaTypes.ts
@@ -0,0 +1,19 @@
+export interface MKMapVotes {
+  fantastic: number
+  beautiful: number
+  good: number
+  bad: number
+  poor: number
+  waste: number
+}
+
+export interface MKVote {
+  readonly mapId: string
+  readonly login: string
+  vote: -3 | -2 | -1 | 1 | 2 | 3
+}
+
+export interface MKVotesInfo {
+  readonly votes: MKMapVotes
+  readonly karma: number
+}
