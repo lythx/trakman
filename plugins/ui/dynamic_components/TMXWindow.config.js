d1622217519dc79321636468bd5a7e48d8304183
lythx <lythxxx@gmail.com>
2022-08-27 23:25:56 +0200

    tmxwinow finished and bugfixes

--- plugins/ui/dynamic_components/TMXWindow.config.js
+++ plugins/ui/dynamic_components/TMXWindow.config.js
@@ -1,3 +1,5 @@
+import icons from '../config/Icons.json' assert { type: 'json' }
+
 export default {
   icon: 'placeholder',
   title: 'Map Info',
@@ -5,6 +7,7 @@ export default {
   itemsPerPage: 3,
   queueCount: 4,
   historyCount: 4,
+  localsCount: 5,
   defaultText: '--',
   defaultTime: '-:--.--',
   titles: {
@@ -12,37 +15,69 @@ export default {
     current: 'Ongoing',
     next: 'Next'
   },
-  noScreenshot: 'placeholder',
+  noScreenshot: icons.placeholder,
   notLoaded: `Press DEL if the image\n      doesn't appear`,
   margin: 0.15,
+  gridRows: [1.2, 6.7, 1.2, 1.2, 4.5, 4.5],
   icons: {
-    header: 'placeholder',
-    name: 'placeholder',
-    download: 'placeholder',
-    author: 'dasda',
-    authorTime: ''
+    header: icons.ongoingMap,
+    name: icons.tag,
+    download: icons.download,
+    author: icons.person,
+    authorTime: icons.clockAuthor,
+    addDate: icons.calendarPlus,
+    copperPrice: icons.cash,
+    environment: icons.carExplode,
+    mood: {
+      day: icons.moodDay,
+      night: icons.moodNight,
+      sunrise: icons.moodSunrise,
+      sunset: icons.moodSunset
+    },
+    voteRatio: icons.karmaPulse,
+    style: icons.mapStyle,
+    difficulty: {
+      beginner: icons.difficultyEasy,
+      intermediate: icons.difficultyMedium,
+      expert: icons.difficultyHard,
+      lunatic: icons.difficultyExtreme
+    },
+    type: icons.puzzle,
+    leaderboardRating: {
+      normal: icons.leaderboardRating,
+      classic: icons.leaderboardRatingClassic,
+      nadeo: icons.leaderboardRatingNadeo
+    },
+    awards: {
+      normal: icons.trophy,
+      classic: icons.trophyClassic,
+      nadeo: icons.trophyNadeo
+    },
+    routes: icons.mapRoutes,
+    buildDate: icons.tools,
+    game: icons.trackmania,
+    voteCount: icons.peopleKarma,
+    maniaExchange: icons.maniaExchange,
+    downloadGreen: icons.downloadHover,
+    dedimania: icons.chartDedi,
+    tmxAuthor: icons.personBuilder,
+    checkpointsAmount: icons.clockList
   },
-  infoIcons: [
-    'placeholder',
-    'placeholder',
-    'placeholder',
-    'placeholder',
-    'placeholder',
-    'placeholder',
-    'placeholder',
-    'placeholder',
-    'placeholder',
-    'placeholder',
-  ],
   iconBackground: "222C",
   contentBackground: "777C",
   gridBackground: '444A',
-  infosBackground: "FFFA",
-  iconWidth: 2.1,
-  infoIconWidth: 2,
+  info: {
+    background: "FFFA",
+    columnsProportions: [1.3, 1.3, 1, 1],
+    rows: 4,
+    iconWidth: 2,
+    textscale: 0.6
+  },
+  authorTimeWidth: 8,
+  iconWidth: 2,
   textscale: 0.67,
-  infoTextscale: 0.6,
   screenshotWidth: 15,
   tmxRecordCount: 3,
+  tmxColumns: [0.7, 2, 3, 3, 0.7],
   recordTextScale: 0.6
 }
