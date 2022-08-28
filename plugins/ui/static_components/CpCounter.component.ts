8724463f024e3b7d972a683c4588c769c48b114a
VTXSRB <jiapetardefx@gmail.com>
2022-08-28 18:57:11 +0200

    andrew checkpointate

--- plugins/ui/static_components/CpCounter.component.ts
+++ plugins/ui/static_components/CpCounter.component.ts
@@ -1,7 +1,7 @@
 
 
 import StaticComponent from '../StaticComponent.js'
-import { CONFIG, IDS, centeredText, getStaticPosition } from '../UiUtils.js'
+import { CONFIG, IDS, centeredText, getStaticPosition,verticallyCenteredText, rightAlignedText } from '../UiUtils.js'
 import { trakman as tm } from '../../../src/Trakman.js'
 
 export default class CpCounter extends StaticComponent {
@@ -9,14 +9,11 @@ export default class CpCounter extends StaticComponent {
     private readonly bg = CONFIG.static.bgColor
     private readonly width = CONFIG.cpCounter.width
     private readonly height = CONFIG.cpCounter.height
-    private readonly posX = CONFIG.static.leftPosition + CONFIG.marginBig + CONFIG.static.width
-    private readonly posY: number
 
     constructor() {
         super(IDS.cpCounter, 'race')
-        const pos = getStaticPosition('rank')
-        this.posY = pos.y
-        tm.addListener('Controller.PlayerCheckpoint', (info: CheckpointInfo) => {
+        tm.addListener('Controller.PlayerCheckpoint', (info) => {
+            console.log(info.index)
             this.displayToPlayer(info.player.login, info.index + 1)
         })
         tm.addListener('TrackMania.PlayerFinish', (params: any[]) => {
@@ -32,15 +29,15 @@ export default class CpCounter extends StaticComponent {
         let xml: string = ''
 
         if (cps === 0) {
-            xml += centeredText(`${tm.utils.palette.tmGreen}No CPs`, this.width, this.height, { yOffset: 1 })
+            xml += centeredText(`${tm.utils.palette.tmGreen}No CPs`, this.width, this.height)
         } else {
-            xml += centeredText('0' + '/' + cps.toString(), this.width, this.height, { yOffset: 1 })
+            xml += rightAlignedText('0' + '/' + cps.toString(), this.width, this.height)
         }
 
         tm.sendManialink(`
     <manialink id="${this.id}">
-        <frame posn="${this.posX} ${this.posY} 1">
-        ${centeredText('CPS', this.width, this.height, { yOffset: -1.5 })}
+        <frame posn="12 -40 1">
+        ${verticallyCenteredText('CPS', this.width, this.height)}
         ${xml}
             <quad posn="0 0 1" sizen="${this.width} ${this.height}" bgcolor="${this.bg}"/>
         </frame>
@@ -54,17 +51,17 @@ export default class CpCounter extends StaticComponent {
         let xml: string = ''
 
         if (cps === 0) {
-            xml += centeredText(`${tm.utils.palette.tmGreen}No CPs`, this.width, this.height, { yOffset: 1 })
+            xml += centeredText(`${tm.utils.palette.tmGreen}No CPs`, this.width, this.height)
         } else if (params === cps) {
-            xml += centeredText(tm.utils.palette.tmGreen + params + '/' + cps, this.width, this.height, { yOffset: 1 })
+            xml += rightAlignedText(tm.utils.palette.tmGreen + params + '/' + cps, this.width, this.height)
         } else {
-            xml += centeredText('0' + '/' + cps.toString(), this.width, this.height, { yOffset: 1 })
+            xml += rightAlignedText(params + '/' + cps.toString(), this.width, this.height)
         }
 
         tm.sendManialink(`
         <manialink id="${this.id}">
-            <frame posn="${this.posX} ${this.posY} 1">
-            ${centeredText('CPS', this.width, this.height, { yOffset: -1.5 })}
+            <frame posn="12 -40 1">
+            ${verticallyCenteredText('CPS', this.width, this.height)}
             ${xml}
                 <quad posn="0 0 1" sizen="${this.width} ${this.height}" bgcolor="${this.bg}"/>
             </frame>
