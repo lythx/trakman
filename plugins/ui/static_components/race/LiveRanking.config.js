97a7545fc2951e72a82e159dc1f93550589b2abb
lythx <lythxxx@gmail.com>
2022-08-31 21:02:25 +0200

    fix liveranking hardcoded maxrecords

--- plugins/ui/static_components/race/LiveRanking.config.js
+++ plugins/ui/static_components/race/LiveRanking.config.js
@@ -9,5 +9,6 @@ export default {
   margin: cfg.margin,
   entries: 6,
   topCount: 5,
-  displayNoRecordEntry: true
+  displayNoRecordEntry: true,
+  maxRecordsAmount: 3000 // If more records than this get driven in one round the click listener will break
 }
