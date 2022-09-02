d70a676d505dcc294e0d378ab397537ae2d32096
wsrvn <vibex2new@gmail.com>
2022-09-03 00:26:25 +0300

    fix some messages

--- plugins/messages/Config.js
+++ plugins/messages/Config.js
@@ -1,9 +1,9 @@
 import { palette as p } from '../../src/Trakman.js'
 
 export default {
-  startup: `${p.servermsg}Trakman ${p.highlight}`
+  startup: `${p.highlight}Trakman`
     + `v#{version}${p.servermsg} startup sequence successful.`,
-  changelog: `${p.server}» ${p.error}You can see the recent changes with` +
+  changelog: `${p.error}You can see the recent changes with` +
     ` the ${p.highlight}/changes ${p.error}command.`,
   noPb: `${p.error}You don't have a personal best on this map.`,
   pb: `${p.record}Personal best${p.highlight}: #{time}${p.record}, the `
@@ -11,19 +11,19 @@ export default {
   noRank: `${p.error}You don't have a rank on the server yet.`,
   rank: `${p.record}You are currently ranked ${p.rank}#{rank} ${p.record}out `
     + `of ${p.highlight}#{total}${p.record} people total.`,
-  welcome: `${p.server}» ${p.error}Welcome to ${p.highlight}#{name}${p.error}. `
-    + `This server is running Trakman ${p.highlight}v#{version}${p.error}.`,
+  welcome: `${p.error}Welcome to ${p.highlight}#{name}${p.error}. `
+    + `This server is running ${p.highlight}Trakman v#{version}${p.error}.`,
   join: `${p.servermsg}#{title}${p.highlight}: `
     + `#{nickname}${p.servermsg} Country${p.highlight}: `
     + `#{country} ${p.servermsg}Visits${p.highlight}: #{visits}${p.servermsg}.`,
-  win: `${p.server}» ${p.record}You have won your `
+  win: `${p.record}You have won your `
     + `${p.rank}#{wins}${p.record} race.`,
-  leave: `${p.server}»» ${p.highlight}#{nickname}${p.servermsg} `
+  leave: `${p.highlight}#{nickname}${p.servermsg} `
     + `has quit after ${p.highlight}#{time}${p.servermsg}.`,
   record: `${p.highlight}#{nickname}${p.record} has `
     + `#{status} the ${p.rank}#{position}${p.record} `
     + `local record. Time${p.highlight}: #{time}#{difference}`,
-  recordDifference: ` ${p.record}$n(${p.rank}#{position} ${p.highlight}-#{time})`,
+  recordDifference: ` ${p.record}$n(${p.rank}#{position} ${p.highlight}-#{time}${p.record})`,
   dediRecord: `${p.highlight}#{nickname}${p.dedirecord} has `
     + `#{status} the ${p.rank}#{position}${p.dedirecord} `
     + `dedimania record. Time${p.highlight}: #{time}#{difference}`
