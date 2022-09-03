28b09968f47a6192667eb0cfe4ef9177facdd697
lythx <lythxxx@gmail.com>
2022-09-03 21:02:52 +0200

    mapcommands config

--- plugins/commands/config/MapCommands.config.js
+++ plugins/commands/config/MapCommands.config.js
@@ -3,7 +3,32 @@ import { palette as p } from '../../../src/Trakman.js'
 export default {
   addallfromdb: {
     error: `${p.error}Failed to get maps from the database.`,
-    public: true,
     privilege: 4
+  },
+  add: {
+    fetchError: `${p.error} Failed to fetch map file from TMX.`,
+    writeError: `${p.error} Failed to write map file.`,
+    queueError: `${p.error} Failed to queue the map.`,
+    alreadyAdded: `${p.admin}Map ${p.highlight}#{map}${p.admin} added by #{nickname}${p.admin} is already on the server, ` +
+      `it will be ${p.highlight}queued ${p.admin}instead.`,
+    added: `${p.admin}#{title} ${p.highlight}#{nickname}${p.admin} has added and queued ${p.highlight}#{map}${p.admin} from TMX.`,
+    public: true,
+    privilege: 1
+  },
+  addlocal: {
+    addError: `${p.error} Failed to add the map.`,
+    added: `${p.admin}#{title} ${p.highlight}#{nickname}${p.admin} has added and queued ${p.highlight}#{map}${p.admin} from local files.`,
+    public: true,
+    privilege: 3
+  },
+  addfromurl: {
+    fetchError: `${p.error} Failed to fetch map file.`,
+    writeError: `${p.error} Failed to write map file.`,
+    queueError: `${p.error} Failed to queue the map.`,
+    alreadyAdded: `${p.admin}Map ${p.highlight}#{map}${p.admin} added by #{nickname}${p.admin} is already on the server, ` +
+      `it will be ${p.highlight}queued ${p.admin}instead.`,
+    added: `${p.admin}#{title} ${p.highlight}#{nickname}${p.admin} has added and queued ${p.highlight}#{map}${p.admin} from url.`,
+    public: true,
+    privilege: 1
   }
 }
