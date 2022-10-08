22ee7c312fcad7128f1d9b33ed8f6bf225f69498
lythx <lythxxx@gmail.com>
2022-10-08 20:23:49 +0200

    averages and blacklist bugfix

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 export default {
   dbUser: `postgres`,
   dbPassword: `ZAQ1@wsx`,
-  dbName: `d`,
+  dbName: `autistic`,
   dbAddress: `127.0.0.1`,
   dbPort: 5432,
   serverOwnerLogin: `ciekma_czakwal`
