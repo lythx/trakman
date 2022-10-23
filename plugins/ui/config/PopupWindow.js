68fac951195793c1f957c4cd8a24aab1e0b36181
wsrvn <vibex2new@gmail.com>
2022-10-23 18:14:20 +0300

    add ui configs

---
+++ plugins/ui/config/PopupWindow.js
@@ -0,0 +1,11 @@
+export default {
+  headerBg: "000C",
+  bg: "000C",
+  margin: 0.15,
+  titleHeight: 5,
+  windowHeight: 60,
+  windowWidth: 90,
+  headerHeight: 4,
+  footerHeight: 4,
+  headerPageWidth: 10
+}
