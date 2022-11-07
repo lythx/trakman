bb762d53dfb99354c5c8805f6cacdb6c7e119997
lythx <lythxxx@gmail.com>
2022-11-07 21:46:04 +0100

    votes stuff

--- plugins/commands/src/KarmaCommands.ts
+++ plugins/commands/src/KarmaCommands.ts
@@ -9,26 +9,27 @@ const options = {
   '---': -3
 } as const
 
-const processVote = (info: tm.MessageInfo): void => {
-  const voteValue = options[info.text as keyof typeof options]
-  if (voteValue !== undefined) {
-    tm.karma.add(info, voteValue)
-    tm.sendMessage(tm.utils.strVar(config.message, {
-      nickname: info.nickname,
-      voteText: config.voteTexts[info.text as keyof typeof config.voteTexts]
-    }), config.public === true ? undefined : info.login)
-  }
+const processVote = (vote: '---' | '--' | '-' | '+' | '++' | '+++', info: tm.MessageInfo): void => {
+  const voteValue = options[vote as keyof typeof options]
+  if (voteValue === undefined ||
+    voteValue === tm.karma.current.find(a => a.login === info.login)?.vote) { return }
+  tm.karma.add(info, voteValue)
+  tm.sendMessage(tm.utils.strVar(config.message, {
+    nickname: tm.utils.strip(info.nickname),
+    voteText: config.voteTexts[vote as keyof typeof config.voteTexts]
+  }), config.public === true ? undefined : info.login)
 }
 
-tm.addListener('PlayerChat', (info): void => { processVote(info) })
+tm.addListener('PlayerChat', (info): void => {
+  processVote(info.text as keyof typeof options, info)
+})
 
 tm.commands.add(
   {
     aliases: ['+++', '++', '+', '-', '--', '---'],
     help: 'Vote for a map.',
-    params: [],
-    callback: async (info: tm.MessageInfo): Promise<void> => {
-      processVote(info)
+    callback: async (info): Promise<void> => {
+      processVote(info.aliasUsed as keyof typeof options, info)
     },
     privilege: 0,
   }
