307422861ae1d46abb30e46eaef1b05180438ef7
wsrvn <105176370+wsrvn@users.noreply.github.com>
2022-12-10 14:21:17 +0300

    assume unchagned....

--- plugins/vote/Config.js
+++ plugins/vote/Config.js
@@ -1,5 +1,17 @@
 export default {
   yesKey: 'F5', // keys can be either F5 F6 or F7
-  noKey: 'F6' ,
-  keyListenerImportance: 10
+  noKey: 'F6',
+  keyListenerImportance: 10,
+  commands: {
+    yes: {
+      aliases: ['y', 'yes'],
+      // help: `Vote "Yes".`,
+      privilege: 0
+    },
+    no: {
+      aliases: ['n', 'no'],
+      // help: `Vote "No".`,
+      privilege: 0
+    }
+  }
 }
