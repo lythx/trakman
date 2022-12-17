ebc4aada6274d1a57fdbc7d2bb2814ae24257c3b
lythx <lythxxx@gmail.com>
2022-12-17 12:09:37 +0100

    column formatting serverinfo

--- plugins/ui/dynamic_components/ServerInfo.component.ts
+++ plugins/ui/dynamic_components/ServerInfo.component.ts
@@ -2,94 +2,104 @@ import { Grid, componentIds, GridCellFunction, GridCellObject, centeredText, clo
 import config from './ServerInfo.config.js'
 import * as os from 'node:os'
 import * as process from 'node:process'
+import 'dotenv/config'
 
 export default class ServerInfoWindow extends PopupWindow {
 
-    private readonly grid: Grid
+  private readonly grid: Grid
 
-    constructor() {
-        super(componentIds.serverInfoWindow, config.icon, config.title, config.navbar)
-        tm.commands.add({
-            aliases: config.command.aliases,
-            help: config.command.help,
-            callback: (info): void => {
-                tm.openManialink(this.openId, info.login)
-            },
-            privilege: config.command.privilege
-        })
-        this.grid = new Grid(this.contentWidth, this.contentHeight, config.columnProportions, new Array(config.cells.length / 2).fill(1), config.grid)
+  constructor() {
+    super(componentIds.serverInfoWindow, config.icon, config.title, config.navbar)
+    tm.commands.add({
+      aliases: config.command.aliases,
+      help: config.command.help,
+      callback: (info): void => {
+        tm.openManialink(this.openId, info.login)
+      },
+      privilege: config.command.privilege
+    })
+    this.grid = new Grid(this.contentWidth, this.contentHeight, config.columnProportions,
+      new Array((config.serverCells.length + config.hostCells.length) / 2 + 1).fill(1), config.grid)
+  }
+
+  private async getServerInfo(): Promise<string[]> {
+    // Dedicated server
+    const dedicatedUptime = `` // TODO
+    const dedicatedVersion = tm.config.server.version
+    const dedicatedBuild = tm.config.server.build
+    // Configurable (or not) server variables
+    const serverName = tm.config.server.name
+    const serverLogin = tm.config.server.login
+    const serverZone = tm.config.server.zone
+    const serverRights = tm.config.server.isUnited ? `United` : `Nations`
+    const serverMaxPlayers = tm.config.server.currentMaxPlayers.toString()
+    const serverMaxSpecs = tm.config.server.currentMaxSpectators.toString()
+    const serverMaxRecs = tm.config.controller.localRecordsLimit.toString()
+    const serverMapCount = tm.maps.count.toString()
+    const serverVisitorsCount = tm.players.count.toString()
+    return [dedicatedUptime, dedicatedVersion, dedicatedBuild, serverName, serverLogin,
+      serverZone, serverRights, serverMaxPlayers, serverMaxSpecs, serverMaxRecs,
+      serverMapCount, serverVisitorsCount]
+  }
+
+  private async getHostInfo(): Promise<string[]> {
+    // Host system information
+    const osUptime = os.uptime().toString() // Seconds
+    const osArch = os.arch()
+    const osCPU = os.cpus() // TODO
+    const osCPULoad = os.cpus() // TODO
+    const osRAM = String(os.totalmem() - os.freemem()) // Bytes
+    const osKernel = os.platform().toUpperCase()
+    // Trakman information
+    const trakmanVersion = tm.config.controller.version
+    const trakmanUptime = ~~process.uptime() // Seconds
+    // Node information
+    const nodeVersion = process.version
+    const nodeRAMUsage = process.memoryUsage()
+    // Postgres information
+    const postgresVersion = await tm.db.query(`select version();`)
+    const postgresDBSize = await tm.db.query(`select pg_size_pretty(pg_database_size(
+     '${process.env.DB_NAME}'));`)
+    return [osUptime, osArch, osCPU, osCPULoad,
+      osRAM, osKernel, trakmanVersion, trakmanUptime, nodeVersion, nodeRAMUsage,
+      postgresVersion, postgresDBSize] as string[]
+  }
+
+  protected async constructContent(): Promise<string> {
+    const serverInfo = await this.getServerInfo()
+    const hostInfo = await this.getHostInfo()
+    const headers: GridCellObject[] = [
+      {
+        background: config.grid.headerBackground,
+        colspan: 2,
+        callback: (i, j, w, h) => centeredText(` Server Information `, w, h),
+      },
+      {
+        background: config.grid.headerBackground,
+        colspan: 2,
+        callback: (i, j, w, h) => centeredText(` Host Information `, w, h),
+      },
+    ]
+    const nameCell: GridCellObject = {
+      background: config.nameColumnBackground,
+      callback: (i, j, w, h): string => {
+        const arr = j === 2 ? config.hostCells : config.serverCells
+        return centeredText(arr[i - 1], w, h)
+      }
     }
-
-    private async getInfo() {
-        // Dedicated server
-        const dedicatedUptime = `` // TODO
-        const dedicatedVersion = tm.config.server.version
-        const dedicatedBuild = tm.config.server.build
-        // Configurable (or not) server variables
-        const serverName = tm.config.server.name
-        const serverLogin = tm.config.server.login
-        const serverZone = tm.config.server.zone
-        const serverRights = tm.config.server.isUnited ? `United` : `Nations`
-        const serverMaxPlayers = tm.config.server.currentMaxPlayers
-        const serverMaxSpecs = tm.config.server.currentMaxSpectators
-        const serverMaxRecs = tm.config.controller.localRecordsLimit
-        const serverMapCount = tm.maps.count
-        const serverVisitorsCount = tm.players.count
-        // Host system information
-        const osUptime = os.uptime() // Seconds
-        const osArch = os.arch()
-        const osCPU = os.cpus() // TODO
-        const osCPULoad = os.cpus() // TODO
-        const osRAM = os.totalmem() - os.freemem() // Bytes
-        const osKernel = os.platform().toUpperCase()
-        // Trakman information
-        const trakmanVersion = tm.config.controller.version
-        const trakmanUptime = ~~process.uptime() // Seconds
-        // Node information
-        const nodeVersion = process.version
-        const nodeRAMUsage = process.memoryUsage()
-        // Postgres information
-        const postgresVersion = await tm.db.query(`select version();`)
-        const postgresDBSize = await tm.db.query(`select pg_size_pretty(pg_database_size(${await tm.db.query(`select current_database();`)}));`)
-
-        return [
-            dedicatedUptime, dedicatedVersion, dedicatedBuild, serverName, serverLogin,
-            serverZone, serverRights, serverMaxPlayers, serverMaxSpecs, serverMaxRecs,
-            serverMapCount, serverVisitorsCount, osUptime, osArch, osCPU, osCPULoad,
-            osRAM, osKernel, trakmanVersion, trakmanUptime, nodeVersion, nodeRAMUsage,
-            postgresVersion, postgresDBSize
-        ]
+    const infoCell: GridCellFunction = (i, j, w, h): string => {
+      const arr = j === 3 ? hostInfo : serverInfo
+      return centeredText(arr[i - 1], w, h)
     }
-
-    protected async constructContent(login: string, params?: any, privilege?: number | undefined): Promise<string> {
-        const info = await this.getInfo()
-        const headers: GridCellObject[] = [
-            {
-                background: config.grid.headerBackground,
-                colspan: 2,
-                callback: (i, j, w, h) => centeredText(` Server Information `, w, h),
-            },
-            {
-                background: config.grid.headerBackground,
-                colspan: 2,
-                callback: (i, j, w, h) => centeredText(` Host Information `, w, h),
-            },
-        ]
-        const nameCell: GridCellFunction = (i, j, w, h): string => {
-            return centeredText(config.cells[i - 1], w, h)
-        }
-        const infoCell: GridCellFunction = (i, j, w, h): string => {
-            return centeredText((info as any)[i - 1], w, h)
-        }
-        const rows = config.cells.length
-        const arr: (GridCellObject | GridCellFunction)[] = headers
-        for (let i = 0; i < rows; i++) {
-            arr.push(nameCell, infoCell)
-        }
-        return this.grid.constructXml(arr)
+    const rows = config.serverCells.length + config.hostCells.length
+    const arr: (GridCellObject | GridCellFunction)[] = headers
+    for (let i = 0; i < rows; i++) {
+      arr.push(nameCell, infoCell)
     }
+    return this.grid.constructXml(arr)
+  }
 
-    protected constructFooter(login: string, params?: any, privilege?: number | undefined): string | Promise<string> {
-        return closeButton(this.closeId, this.windowWidth, this.footerHeight)
-    }
+  protected constructFooter(): string | Promise<string> {
+    return closeButton(this.closeId, this.windowWidth, this.footerHeight)
+  }
 }
--- plugins/ui/dynamic_components/ServerInfo.config.js
+++ plugins/ui/dynamic_components/ServerInfo.config.js
@@ -2,56 +2,59 @@ import ids from '../config/ComponentIds.js'
 import icons from '../config/Icons.js'
 
 export default {
-    title: `Server Overview`,
-    icon: icons.placeholder,
-    navbar: [
-        { name: `Changelog`, actionId: ids.changelog }
-    ],
-    grid: {
-        background: "9996",
-        margin: 0.15,
-        headerBackground: "333C"
-    },
-    columnProportions: [
-        3,
-        2,
-        3,
-        2
-    ],
-    textScale: 1.4,
-    tileBackground: "9996",
-    marginBig: 1,
-    entries: 4,
-    headerHeight: 3,
-    cells: [
-        ` Dedicated Server Uptime `,
-        ` Dedicated Server Version `,
-        ` Dedicated Server Build `,
-        ` Server Name `,
-        ` Server Login `,
-        ` Server Zone `,
-        ` Server Rights `,
-        ` Server Max Players `,
-        ` Server Max Spectators `,
-        ` Server Max Records `,
-        ` Server Map Count `,
-        ` Server Visitor Count`,
-        ` System Uptime `,
-        ` System Architecture `,
-        ` System CPU `,
-        ` System CPU Load `,
-        ` System RAM `,
-        ` System Kernel `,
-        ` Trakman Version `,
-        ` Trakman Uptime `,
-        ` NodeJS Version `,
-        ` NodeJS RAM Usage `,
-        ` PostgreSQL Version `,
-        ` PostgreSQL DB Size `,
-    ],
-    command: {
-        aliases: ['server', 'serverinfo', 'sinfo'],
-        help: `Display the detailed server information.`,
-        privilege: 0
-    }
+  title: `Server Overview`,
+  icon: icons.placeholder,
+  navbar: [
+    { name: `Changelog`, actionId: ids.changelog }
+  ],
+  grid: {
+    background: "9996",
+    margin: 0.15,
+    headerBackground: "333C"
+  },
+  nameColumnBackground: '5559',
+  columnProportions: [
+    2,
+    2,
+    2,
+    2
+  ],
+  textScale: 1.4,
+  tileBackground: "9996",
+  marginBig: 1,
+  entries: 4,
+  headerHeight: 3,
+  serverCells: [
+    ` Dedicated Server Uptime `,
+    ` Dedicated Server Version `,
+    ` Dedicated Server Build `,
+    ` Server Name `,
+    ` Server Login `,
+    ` Server Zone `,
+    ` Server Rights `,
+    ` Server Max Players `,
+    ` Server Max Spectators `,
+    ` Server Max Records `,
+    ` Server Map Count `,
+    ` Server Visitor Count`,
+  ],
+  hostCells: [
+    ` System Uptime `,
+    ` System Architecture `,
+    ` System CPU `,
+    ` System CPU Load `,
+    ` System RAM `,
+    ` System Kernel `,
+    ` Trakman Version `,
+    ` Trakman Uptime `,
+    ` NodeJS Version `,
+    ` NodeJS RAM Usage `,
+    ` PostgreSQL Version `,
+    ` PostgreSQL DB Size `
+  ],
+  command: {
+    aliases: ['server', 'serverinfo', 'sinfo'],
+    help: `Display the detailed server information.`,
+    privilege: 0
+  }
 }
