4965bbb28f2ca7777295e2c81fac6b1bd91ce621
lythx <lythxxx@gmail.com>
2023-04-06 19:22:55 +0200

    remove ui newutils

--- plugins/ui/newutils/View.ts
+++
@@ -1,7 +0,0 @@
-import { QuadNode } from "./QuadNode.js";
-import { Styles } from "./StyleSheet.js";
-import { UiNode } from "./UiNode.js";
-
-export const View = (styles: Styles = {}, ...children: UiNode[]) => {
-  return QuadNode(styles, ...children)
-}
