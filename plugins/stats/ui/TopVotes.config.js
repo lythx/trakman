c7a81b413155d084c00af6246ef77f7389cbf8df
wsrvn <vibex2new@gmail.com>
2023-04-25 19:04:48 +0300

    not everything is cash..

--- plugins/stats/ui/TopVotes.config.js
+++ plugins/stats/ui/TopVotes.config.js
@@ -2,7 +2,7 @@ import { icons, componentIds as ids } from '../../ui/UI.js'
 
 export default {
   title: "Top Votes",
-  icon: icons.cash,
+  icon: icons.karmaStats,
   gridColumns: [0.8, 4, 3, 3, 0.8, 4, 3, 3],
   headers: ['Lp.', 'Nickname', 'Login', 'Votes', 'Lp.', 'Nickname', 'Login', 'Votes'],
   entries: 30, // Has to be even number
