8cfce1b0850e0f8fb9752d37d389752e3270ebc4
lythx <lythxxx@gmail.com>
2024-02-13 16:16:52 +0100

    fix dedimania config comment

--- plugins/dedimania/Config.js
+++ plugins/dedimania/Config.js
@@ -4,8 +4,8 @@ export default {
   isEnabled: true,
   // Maximum amount of fetched records 
   // (Dedimania normally stores 30, there are exceptions such as Nadeo maps with 50-80 records)
-  syncName: true, // If true, sets player nicknames in the database to those fetched from Dedimania
   dediCount: 30,
+  syncName: true, // If true, sets player nicknames in the database to those fetched from Dedimania
   port: 8002, // Dedimania port (8002 for TMF)
   host: 'dedimania.net',
   reconnectTimeout: 120, // Timeout for Dedimania reconnection attempts, in seconds
